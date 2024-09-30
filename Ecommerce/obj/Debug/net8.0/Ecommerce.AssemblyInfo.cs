2976ed0f233cef7a1a7bd6b1feb8f2b50820d462
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2024-09-30 10:50:10 +0530

    all network access allowed

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54780cfd6c75704c0ed8500b1b8b546c977d5c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f594010d79d4ae90e393c0108904db31d33414e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
