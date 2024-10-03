7630ebec0025f9c88384f9ea1c064d9b249195aa
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2024-10-03 09:05:01 +0530

    add cross origin dependancy

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd55e3bd3e2d152035e6b3cca4458622b9cd49f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1b27c3584d896f4f574d317304a95fca69bf3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
