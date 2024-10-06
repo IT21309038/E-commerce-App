316eeefce7365690865a7edea10f9e7ef7606492
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2024-10-06 22:45:23 +0530

    order dto created to custom get response

---
+++ Ecommerce/DTO/OrderGetDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce.DTO
+{
+    public class OrderGetDTO
+    {
+        public string Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderStatus { get; set; }
+        public bool EditableStatus { get; set; }
+        public bool CancelStatus { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string CustomerId { get; set; }
+        public List<OrderItemDTO> OrderItems { get; set; }
+    }
+}
